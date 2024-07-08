9defffc9b0964fdaf25754cf6c39f8a8ce807f91
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-07-09 00:41:19 +0300

    task 2 ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+   </a>
+   </li>`
+  )
+  .join("");
+
+ulEl.insertAdjacentHTML("beforeend", markup);
+
+ulEl.addEventListener("click", onModal);
+
+function onModal(evt) {
+  evt.preventDefault();
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    caption: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+console.log(SimpleLightbox);
